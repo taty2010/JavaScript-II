b5a5613e611027d6b9781db71292d78480f61425
Tatyana <43764894+taty2010@users.noreply.github.com>
2019-08-13 09:00:09 -0500

    Update callbacks.js
    completed the main problems of the assignment

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,57 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(items.length);
 }
+
+ getLength(items, function(length) {
+    console.log(length)
+  });
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(items[3]);
 }
+
+last(items, function(lastItem){
+     console.log(lastItem);
+     });
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+ const add = function(x, y){
+  return x + y;
+}
+
+console.log(sumNums(5, 8, add));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+const multiply = function (x, y){
+  return x * y;
+}
+
+console.log(multiplyNums(10, 10, multiply))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb = list.includes(item)
 }
+
+const itemList = function(item, list){
+  return itemList ;
+}
+
+console.log(contains("Gum", items, itemList));
 
 /* STRETCH PROBLEM */
 
